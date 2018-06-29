cdf220c9d1042e0f112abae47e6ba48a735d0099
AminIsaev <amin.isaev@gmail.com>
2018-06-29 23:10:34 +0700

    index:

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const Discord = require("discord.js");
+const prefix = "ash ";
 bot = new Discord.Client();
 
 bot.on("ready", async() =>{
   console.log(`${bot.user.username} готов!`);
+});
+
+bot.on("message", async message =>{
+if(message.author.bot) return;
+
+let messageArray = message.content.split(" ");
+let cmd = messageArray[0];
+let args = messageArray[1].slice(1);
+
+if(cmd === `${prefix}ping`){
+  message.reply("pong");
+}
 });
 
 bot.login(process.env.TOKEN);
