b0e5c6b1bd514143234de616b215f44e178cfa57
AminIsaev <amin.isaev@gmail.com>
2018-07-01 20:13:24 +0700

    profile changes

--- commands/profile.js
+++ commands/profile.js
@@ -5,8 +5,20 @@ var mysql = require('mysql');
 
 module.exports.run = async (bot, message, args, con) => {
     con.query('SELECT * FROM users WHERE uid = ?', message.author.id, function(error, result, fields){
-        message.reply(result[0].about);
-    });
+        var row = result[0];
+        if(row.duel_win > 0 || row.duel_lose > 0){ var perc = (row.duel_win/(row.duel_win+row.duel_lose)).toFixed(1); }
+        else { var perc = 0; }
+        if(row.xp%100 < 10) var le = ''+row.xp%100;else var le = row.xp%100;
+        let profileEmbed = new Discord.RichEmbed()
+        .setDescription(row.xp+"/"+(Math.floor(row.xp/100)+1)+'00 XP')
+        .setAuthor(message.author.username)
+        .setTitle("LVL "+Math.floor(row.xp/100)
+        .setThumbnail(message.author.avatarURL)
+        .setColor("#15f153")
+        .addField("Поинты", row.points)
+        .addField("W/L", row.duel_win+'/'+row.duel_lose+' - '+perc*100+'%')
+        .addField("Обо мне", row.about)
+        });
 }
 
 module.exports.help = {
