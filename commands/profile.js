e5c1364bcadad8b4c26a5dc66f10a7cd5ef8bf20
AminIsaev <amin.isaev@gmail.com>
2018-07-01 19:48:13 +0700

    mysql

---
+++ commands/profile.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "sql9.freemysqlhosting.net",
+  user: "sql9245488",
+  password: "rnLl6FcxIE",
+  database: "sql9245488"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+module.exports.run = async (bot, message, args) => {
+    con.query(`SELECT * FROM users WHERE uid = ${message.author.id}`, function(error, result, fields){
+        message.reply(result);
+    });
+}
+
+module.exports.help = {
+    name: "profile"
+}
